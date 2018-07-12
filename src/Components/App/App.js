cb6c060fa9074e6e9ebeddffe9089aae133993da
James <nurse.djm@gmail.com>
2018-07-12 16:52:01 -0600

    start passing down the dearch result

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,19 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      searchResults: [
+        {name: "n"},
+        {artist: "a"},
+        {album: "al"},
+        {id: "id"}
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
@@ -12,7 +25,7 @@ class App extends Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults} />
             <Playlist />
           </div>
         </div>
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,7 +6,13 @@ class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-       //-- You will add a map method that renders a set of Track components
+        {
+          this.props.tracks.map(track => {
+            return <Track
+               track={track}
+               key={track.id} />
+            })
+        }
       </div>
     )
   }
